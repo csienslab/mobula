67c83494289d96e8d941e93ccb99789d67884341
PZ Read <netfirewall@gmail.com>
2019-03-01 18:51:17 +0000

    Add tunnel

--- lib/gateway.sh
+++ lib/gateway.sh
@@ -17,12 +17,20 @@ ip link set ${GW_EXTIF} up
 ip route add default via ${EXT_GATEWAY}
 
 # Setup the iptables and enable forwarding
-iptables -t nat -A POSTROUTING -o ${GW_EXTIF} -j MASQUERADE
-iptables -t nat -A PREROUTING -i ${GW_EXTIF} -p tcp --dport 22 -j DNAT --to-destination ${VETH_M0_IP}:22
+modprobe nf_conntrack_proto_gre
 echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o ${GW_EXTIF} -j MASQUERADE
+iptables -t nat -A POSTROUTING -o ${GW_WGIF} -j MASQUERADE
+iptables -t nat -A PREROUTING -i ${GW_EXTIF} -p tcp --dport 22 -j DNAT --to-destination ${VETH_M0_IP}:22
+
+# Enhance the firewall
+iptables -A INPUT -i ${GW_EXTIF} -j DROP
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i ${GW_EXTIF} -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -i ${GW_EXTIF} -j DROP
 
 # Setup the WireGuard
-wg-quick up ${BASEDIR}/wg0.conf
+wg-quick up ${BASEDIR}/${GW_WGIF}.conf
 
 # Test and update the ARP of the external gateway
 ping -W 6 -c 10 ${EXT_GATEWAY} &
